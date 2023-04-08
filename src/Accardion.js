c7de92bdc4ce9b9e083e7c82ead08c5757cc0632
atefehsohrabi <50141900+atefehsohrabi@users.noreply.github.com>
2023-04-08 13:16:29 -0700

    edit

--- src/Accardion.js
+++ src/Accardion.js
@@ -1,43 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import {Accordion} from 'react-bootstrap';
 
+
+const generateAccordion = (data)=>{
+    return(
+        <Accordion defaultActiveKey='0' >
+
+            {data?.map((item, index) => (
+                
+                    <Accordion.Item eventKey={index} key={item.name} className='item'>
+                        <Accordion.Header>
+                            {item.name}
+                        </Accordion.Header>
+                        <Accordion.Body>
+                         
+                            {item?.children?.length>0
+                            ?generateAccordion(item.children)
+                            :null}
+                                
+                                    </Accordion.Body>
+                                </Accordion.Item>
+            ))}
+
+        </Accordion>
+    );
+};
+
+
 const Accardion=()=> {
-    const[products,setProduct]=useState()
+    const[products,setProduct]=useState([]);
 useEffect(()=>{
     fetch('https://mohsenfathipour.com/api/categories.json')
     .then(Response=>Response.json())
-    .then(data=>setProduct(data))
-},[])
-    return (
-        <Accordion defaultActiveKey='0' className='mt-5 p-3'>
-
-            {products.map(product => (
-                <>
-                    <Accordion.Item eventKey='0'  className='item'>
-                        <Accordion.Header>
-                            {product.name}
-                        </Accordion.Header>
-                        <Accordion.Body>
-                            return(
-                            {products.children.map(child => (<>
-                                <Accordion.Item eventKey='0'  className='item'>
-                                    <Accordion.Header>
-                                        {child.name}
-                                    </Accordion.Header>
-                                    <Accordion.Body>
-                                    </Accordion.Body>
-                                </Accordion.Item>
-                            </>))}
-                            )
-                           
-                        </Accordion.Body>
-                    </Accordion.Item>
-                </>
-               
-            ))}
+    .then(data=>setProduct(data));
+},[]);
+    return <div className="mt-5 p-3">{generateAccordion(products)}</div>
         
-     </Accordion>
-    );
-}
-
+};
 export default Accardion;
